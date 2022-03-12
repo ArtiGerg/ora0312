0e28e619753d715d979cd490a150f12475a9efcd
ArtiGerg <artemontth@gmail.com>
2022-03-12 13:18:05 +0100

    Initial commit

---
+++ src/ora0312.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class ora0312 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("CSAK");
+
+    }
+}
+
