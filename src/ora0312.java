20ea29b34faf4e9816ff28efe21feea2297ac1a5
ArtiGerg <artemontth@gmail.com>
2022-03-12 17:40:30 +0100

    saff

--- src/ora0312.java
+++ src/ora0312.java
@@ -4,9 +4,32 @@ public class ora0312 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Kérem az első medve életerejét es sebzését");
+    int hp1= scanner.nextInt();
+    scanner.nextLine();
+    int sebzes1= scanner.nextInt();
+        System.out.println("Kérem a Második medve életerejét es sebzését");
+        int hp2= scanner.nextInt();
+        scanner.nextLine();
+        int sebzes2= scanner.nextInt();
+scanner.nextLine();
+        while (hp1>0 || hp2>0){
+           hp1=hp1 - sebzes2;
+           hp2=hp2-sebzes1;
+        }if (hp1<0){
+            System.out.println("A második medve nyert");
+        }if (hp2<0){
+            System.out.println("Az első medve nyert");
+        }
+        else{
+            System.out.println("döntetlen");
+        }
 
-        System.out.println("CSAK");
-
+        }
     }
-}
+
+
+
+
 
